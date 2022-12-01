d21e4ea460c11a4522045e7bfc7a748ad96ab2f8
Griswald Brooks <griswald.brooks@gmail.com>
2022-12-01 18:20:56 +0000

    Read file

--- dec01/src/lib.rs
+++ dec01/src/lib.rs
@@ -1,32 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use std::fs::File;
-    use std::io::prelude::*;
-    use std::path::Path;
-    use std::path::PathBuf;
-
 
     #[test]
     fn it_works() {
-        // Create a path to the desired file
-        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        d.push("resources/test");
-
-        let path = Path::new(d + "input.txt");
-        let display = path.display();
-
-        // Open the path in read-only mode, returns `io::Result<File>`
-        let mut file = match File::open(&path) {
-            Err(why) => panic!("couldn't open {}: {}", display, why),
-            Ok(file) => file,
-        };
-
-        // Read the file contents into a string, returns `io::Result<usize>`
-        let mut s = String::new();
-        match file.read_to_string(&mut s) {
-            Err(why) => panic!("couldn't read {}: {}", display, why),
-            Ok(_) => print!("{} contains:\n{}", display, s),
-        }
+        let expected = std::fs::read_to_string("resources/test/input.txt").expect("Invalid file");
+        println!("{}", expected);
 
         let result = 2 + 2;
         assert_eq!(result, 4);
