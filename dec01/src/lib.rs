e330b24ee20400e9656cdcd24cdaf5a458952936
Griswald Brooks <griswald.brooks@gmail.com>
2022-12-02 22:31:39 +0000

    Cleanup

--- dec01/src/lib.rs
+++ dec01/src/lib.rs
@@ -3,8 +3,11 @@ mod tests {
     #[test]
     fn it_works() {
         let input = std::fs::read_to_string("resources/test/input.txt").expect("Invalid file");
-        let calories = input.split("\n\n").map(|groceries| -> i32 {
-            groceries.split('\n').map(|calorie| calorie.parse::<i32>().unwrap()).sum::<i32>()    
+        let calories = input.split("\n\n").map(|groceries| {
+            groceries
+                .split('\n')
+                .flat_map(|calorie| calorie.parse::<i32>())
+                .sum()
         });
         assert_eq!(calories.max(), Some(66616));
     }
