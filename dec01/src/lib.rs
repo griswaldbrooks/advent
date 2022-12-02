05c239a1f1432bc28eb5b8da7acdcfd3a8da1560
Griswald Brooks <griswald.brooks@gmail.com>
2022-12-02 22:06:47 +0000

    Code golf

--- dec01/src/lib.rs
+++ dec01/src/lib.rs
@@ -3,18 +3,9 @@ mod tests {
     #[test]
     fn it_works() {
         let input = std::fs::read_to_string("resources/test/input.txt").expect("Invalid file");
-        let food_calories: Vec<&str> = input.split("\n\n").collect();
-
-        let mut max = 0;
-        for calories in food_calories {
-            let mut sum = 0;
-            for s in calories.split('\n') {
-                sum += s.parse::<i32>().unwrap();
-            }
-            if sum > max {
-                max = sum;
-            }
-        }
-        assert_eq!(max, 66616);
+        let calories = input.split("\n\n").map(|groceries| -> i32 {
+            groceries.split('\n').map(|calorie| calorie.parse::<i32>().unwrap()).sum::<i32>()    
+        });
+        assert_eq!(calories.max(), Some(66616));
     }
 }
