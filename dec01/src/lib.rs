80a9b5e9fb7f488c1f0d76443e393f23c3222fff
Griswald Brooks <griswald.brooks@gmail.com>
2022-12-02 21:17:54 +0000

    Dec 01 solution, messy

--- dec01/src/lib.rs
+++ dec01/src/lib.rs
@@ -1,12 +1,20 @@
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn it_works() {
-        let expected = std::fs::read_to_string("resources/test/input.txt").expect("Invalid file");
-        println!("{}", expected);
+        let input = std::fs::read_to_string("resources/test/input.txt").expect("Invalid file");
+        let food_calories: Vec<&str> = input.split("\n\n").collect();
 
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut max = 0;
+        for calories in food_calories {
+            let mut sum = 0;
+            for s in calories.split('\n') {
+                sum += s.parse::<i32>().unwrap();
+            }
+            if sum > max {
+                max = sum;
+            }
+        }
+        assert_eq!(max, 66616);
     }
 }
